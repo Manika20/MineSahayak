a8e3580ce975f9279574a79913303a82a4cf4397
Shobhnik13 <shobhnikw@gmail.com>
2023-09-09 00:00:12 +0530

    initial

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div className='bg-blue-500'>
+        {/* create different components or sections in componentsfolder and reneder here */}
+        {/* just like navbar,header,footer,about,features,faq, etc  */}
+        {/* do not directly write the code of any section here */}
+        MineSahayak
+    </div>
+  )
+}
+
+export default LandingPage
