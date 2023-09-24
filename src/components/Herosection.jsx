a914b6b0d1c37f413f14d041be5e65ab56c3ae3e
Shobhnik13 <shobhnikw@gmail.com>
2023-09-25 00:19:06 +0530

    typewriter and button added

--- src/components/Herosection.jsx
+++ src/components/Herosection.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import VideoLogo from '../assets/Videoicon.png'
+import Navbar from './Navbar';
+import TypewriterComponent from 'typewriter-effect';
 const Herosection = () => {
   return (
-    <section className='bg-gradient-to-b from-indigo-950 to-black h-screen md:h-92 flex justify-center items-center md:items-baseline md:pt-52 '>
+    <div className='bg-gradient-to-b from-indigo-950 to-black h-screen md:h-92'>
+      <Navbar/>
+    <section className=' flex justify-center items-center md:items-baseline pt-28 '>
       {/* main div for heading and button thing  */}
       <div className='flex flex-col items-center justify-center gap-x-4'>
       {/* heading div  */}
@@ -12,6 +16,14 @@ const Herosection = () => {
       {/* button div   */}
       <div className='text-center md:text-start flex flex-col justify-center md:flex-row md:justify-between items-center gap-y-4 md:gap-x-4 p-4 md:px-6 md:border md:border-1  md:border-indigo-400 md:mt-8 rounded-full'>
         {/* headline */}
+        {/* <div  className='text-sm  text-gray-400 font-semibold md:text-xl'> */}
+          {/* <TypewriterComponent
+          options={{
+            strings: ['Hello', 'World'],
+            autoStart: true,
+            loop: true,
+          }}/> */}
+        {/* </div> */}
             <p className='text-sm  text-gray-400 font-semibold md:text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, nobis?</p>
          {/* butto */}
          <button className="btn hover:scale-95 transition-all duration-100 font-extrabold text-base/snug tracking-tight btn-primary bg-gradient-to-r from-blue-600 via-violet-600 to-purple-600">Get Started!</button>         
@@ -23,8 +35,10 @@ const Herosection = () => {
           <p className='text-lg/snug tracking-wide text-transparent bg-clip-text bg-gradient-to-r from-gray-500 via-gray-400 to-gray-300 capitalize font-bold'>Watch tutorial </p>
           </a>
       </div>
+      {/* learn more  */}
     </div>
     </section>
+    </div>
   );
 };
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-between items-center p-4'>
+        {/* logo  */}
+        <div></div>
+        {/* button  */}
+        <button className="btn hover:scale-95 transition-all duration-100 font-extrabold text-base/snug tracking-tight btn-primary bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600">Dive In</button>
+    </div>
+  )
+}
+
+export default Navbar
