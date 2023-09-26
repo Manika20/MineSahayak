913eb427e977d1760294bd6c9ecfeb334c6650ff
cd-vishal <vishal.chauhan@clickdee.com>
2023-09-26 07:48:41 +0530

    sidebar-added

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -23,12 +23,28 @@ const Dashboard = () => {
   }, []);
 
   return (
-    <div className="h-screen bg-gradient-to-b from-indigo-950 to-black">
-    <div className="flex flex-col items-center justify-between h-full">
+    <div className="h-screen bg-gradient-to-b from-indigo-950 to-black flex">
+    {/* Sidebar */}
+    <div className="bg-gray-800 w-1/4 p-4">
+      <h2 className="text-white text-2xl mb-4">History</h2>
+      <ul className="text-white">
+        <li className="mb-2 border-gray-500 border-2 p-1">How to mine in Rainy area...</li>
+        <li className="mb-2 border-gray-500 border-2 p-1">Some hazards...</li>
+        <li className="mb-2 border-gray-500 border-2 p-1">Legal Tenders...</li>
+      
+      </ul>
+    </div>
+    
+    {/* Main Content */}
+    <div className="flex-1 flex flex-col items-center justify-between">
+    <div className="flex justify-between w-full">
+      <div></div>
       <div className="text-5xl mt-5 text-cyan-500">Minesahayak Chatbot</div>
-      <div className="chat-container flex flex-col w-[80vw] justify-end">
+      <div className="w-[40px] bg-red-300 rounded-full p-2 text-center mr-4"></div>
+      </div>
+      <div className="chat-container flex flex-col w-full ">
         <div className="flex justify-start">
-          <div className="bg-blue-500 text-white rounded-lg p-2 my-2 max-w-[80%]">
+          <div className="bg-blue-500 text-white rounded-lg p-2 my-2 max-w-[80%] ">
             Hello! How can I assist you today?
           </div>
         </div>
@@ -37,6 +53,7 @@ const Dashboard = () => {
             I have a question about my account.
           </div>
         </div>
+        {/* Add more chat messages here */}
       </div>
       <div className="input mb-4 w-[80vw] flex flex-row bg-transparent">
         <input
@@ -50,8 +67,6 @@ const Dashboard = () => {
       </div>
     </div>
   </div>
-  
-  
   );
 };
 
