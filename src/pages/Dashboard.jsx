0dc6335927f6f83e6e381bf5699e09e67b2b51e2
manika <goyalmanika20@gmail.com>
2023-10-07 16:27:28 +0530

    dashboard is now responsive

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -31,8 +31,8 @@ const Dashboard = () => {
   
   const MyOffcanvas = () => {
     return (
-      <Offcanvas className="fixed top-0 md:hidden w-1/3 bottom-0 left-0 border bg-gray-800 text-white p-4" show={showOffcanvas} onHide={handleOffcanvas}>
-        <Offcanvas.Header closeButton className="flex justify-between">
+      <Offcanvas className="fixed top-0 md:hidden w-1/2 sm:w-1/3 bottom-0 left-0 border bg-gray-800 text-white p-4" show={showOffcanvas}>
+        <Offcanvas.Header  className="flex justify-between">
         <h2 className="text-white text-3xl mb-4 ">History</h2>
           {/* Close button for dismiss option */}
           <Button className="border-0 bg-transparent  text-white" onClick={handleOffcanvas}>
@@ -100,8 +100,8 @@ const Dashboard = () => {
     <div className="flex-1 flex flex-col items-center justify-between ml-3">
     <div className="flex justify-between w-full">
       <div></div>
-      <div className=" text-3xl md:text-5xl  h-auto md:h-14 mt-5 tracking-tighter text-transparent  bg-clip-text bg-gradient-to-r from-blue-400 via-blue-600 to-blue-800 font-bold">Minesahayak <span className="hidden lg:inline">Chatbot</span></div>
-      <div className="w-[30px] md:w-[50px] bg-yellow-200 rounded-full text-center flex items-center justify-center mr-5  mt-6">U</div>
+      <div className=" text-3xl md:text-5xl  h-10 md:h-14  mt-5 tracking-tighter text-transparent  bg-clip-text bg-gradient-to-r from-blue-400 via-blue-600 to-blue-800 font-bold">Minesahayak <span className="hidden lg:inline">Chatbot</span></div>
+      <div className="w-[40px] md:w-[50px] bg-yellow-200 rounded-full text-center flex items-center justify-center mr-5  mt-6">U</div>
       </div>
       <div className="chat-container flex flex-col w-full  mr-6 md:mr-0">
 
@@ -125,7 +125,7 @@ const Dashboard = () => {
         />
        
         <button className="h-10 flex justify-center items-center text-white w-[100px] hover:scale-95 transition-all duration-100 font-bold tracking-wide  bg-gradient-to-r from-blue-600 via-violet-600 to-purple-600 rounded-r-lg text-lg text-center">
-          Submit
+          <span className="hidden sm:inline">Submit</span>
           <Zap className="text-white w-4 h-4" fill="white"/>
         </button>
         
