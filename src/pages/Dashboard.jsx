9fad239d45e0216bcb4a6a06c430edee95145f65
manika <goyalmanika20@gmail.com>
2023-10-07 11:00:18 +0530

    made dashboard responsive

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { supabase } from "../lib/helper/SupabaseClient";
-import { MessageSquare, Zap } from "lucide-react";
+import { MessageSquare, Zap,Menu,X } from "lucide-react";
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
 const Dashboard = () => {
   const navigate = useNavigate();
@@ -23,54 +26,98 @@ const Dashboard = () => {
     fetchUser();
   }, []);
 
+  const [showOffcanvas, setShowOffcanvas] = useState(false);
+  const handleOffcanvas = () => setShowOffcanvas(!showOffcanvas);
+  
+  const MyOffcanvas = () => {
+    return (
+      <Offcanvas className="fixed top-0 md:hidden w-1/3 bottom-0 left-0 border bg-gray-800 text-white p-4" show={showOffcanvas} onHide={handleOffcanvas}>
+        <Offcanvas.Header closeButton className="flex justify-between">
+        <h2 className="text-white text-3xl mb-4 ">History</h2>
+          {/* Close button for dismiss option */}
+          <Button className="border-0 bg-transparent  text-white" onClick={handleOffcanvas}>
+            <X className= "text-white" fill="white"/>
+          </Button>
+          </Offcanvas.Header>
+        <Offcanvas.Body>
+          {/* Your Offcanvas content goes here */}
+          <ul className="text-white rounded-xl">
+      <div className="flex gap-x-2 items-center justify-left">
+        <MessageSquare className="w-6 h-6"/> 
+        <li className="mb-2 border-b-1 p-1 line-clamp-1 truncate w-24 sm:w-52">How to mine in Rainy area How to mine in Rainy area...</li>
+        </div>
+        <div className="flex gap-x-2 items-center justify-left">
+        <MessageSquare className="w-6 h-6"/> 
+        <li className="mb-2 border-b-1 p-1 line-clamp-1 truncate w-24 sm:w-52">Some hazards...</li>
+        </div>
+
+        <div className="flex gap-x-2 items-center justify-left">
+        <MessageSquare className="w-6 h-6"/> 
+        <li className="mb-2 border-b-1 p-1 line-clamp-1 truncate w-24 sm:w-52">Legal Tenders...</li>
+        </div>
+
+   </ul>
+        </Offcanvas.Body>
+      </Offcanvas>
+    );
+  };
+  
+
   return (
 
     <div className="h-screen bg-gradient-to-b from-indigo-950 to-black flex">
+     {/*menu*/}
+     <div className="md:hidden">
+  <Button className="border-0 mt-4 bg-transparent text-white" onClick={handleOffcanvas}>
+    <Menu className= "text-white" fill="white"/>
+     </Button>
+   <MyOffcanvas />
+    </div>
+     
+     
     {/* Sidebar */}
-    <div className="bg-gray-800 w-1/4 p-4">
+    <div className="hidden md:block bg-gray-800 w-1/4 p-4">
       <h2 className="text-white text-2xl mb-4">History</h2>
       <ul className="text-white rounded-xl">
-        
-    <li className="mb-2 border-b-1 p-1 line-clamp-1 flex gap-x-2 items-center justify-center">
-    <MessageSquare className="w-6 h-6"/> 
-    <span>How to mine in Rainy area...</span>
-    </li>
+      <div className="flex gap-x-2 items-center justify-left">
+        <MessageSquare className="w-6 h-6"/> 
+        <li className="mb-2 border-b-1 p-1 line-clamp-1 truncate md:w-32 lg:w-44 xl:w-80">How to mine in Rainy area How to mine in Rainy area...</li>
+        </div>
+        <div className="flex gap-x-2 items-center justify-left">
+        <MessageSquare className="w-6 h-6"/> 
+        <li className="mb-2 border-b-1 p-1 line-clamp-1 truncate md:w-32 lg:w-44 xl:w-80">Some hazards...</li>
+        </div>
 
-    <li className="mb-2 border-b-1 p-1 line-clamp-1 flex gap-x-2 items-center justify-center"> 
-    <MessageSquare className="w-6 h-6"/> 
-    <span>Some hazards...</span>
-    </li>
+        <div className="flex gap-x-2 items-center justify-left">
+        <MessageSquare className="w-6 h-6"/> 
+        <li className="mb-2 border-b-1 p-1 line-clamp-1 truncate md:w-32 lg:w-44 xl:w-80">Legal Tenders...</li>
+        </div>
 
-   
-      
-
-      
-      
-      </ul>
+   </ul>
     </div>
     
     {/* Main Content */}
     <div className="flex-1 flex flex-col items-center justify-between ml-3">
     <div className="flex justify-between w-full">
       <div></div>
-      <div className="text-5xl mt-5 ttracking-tighter text-transparent bg-clip-text bg-gradient-to-r from-blue-400 via-blue-600 to-blue-800 font-bold">Minesahayak Chatbot</div>
-      <div className="w-[40px] bg-red-300 rounded-full text-center flex items-center justify-center mr-4 mt-6">U</div>
+      <div className=" text-3xl md:text-5xl  h-auto md:h-14 mt-5 tracking-tighter text-transparent  bg-clip-text bg-gradient-to-r from-blue-400 via-blue-600 to-blue-800 font-bold">Minesahayak <span className="hidden lg:inline">Chatbot</span></div>
+      <div className="w-[30px] md:w-[50px] bg-yellow-200 rounded-full text-center flex items-center justify-center mr-5  mt-6">U</div>
       </div>
-      <div className="chat-container flex flex-col w-full ">
+      <div className="chat-container flex flex-col w-full  mr-6 md:mr-0">
 
         <div className="flex justify-start">
-          <div className="bg-blue-500 text-white rounded-lg p-2 my-2 max-w-[80%] ">
+          <div className="bg-blue-500  text-white rounded-lg p-2 my-2 ml-1  max-w-[80%] ">
             Hello! How can I assist you today?
           </div>
         </div>
         <div className="flex justify-end">
-          <div className="bg-gray-300 text-black rounded-lg p-2 my-2 max-w-[80%]">
+          <div className="bg-gray-300 text-black rounded-lg p-2 my-2 mr-4  max-w-[80%]">
             I have a question about my account.
           </div>
         </div>
         {/* Add more chat messages here */}
       </div>
-      <div className="input mb-4 w-[80vw] flex flex-row bg-transparent">
+      <div className="input mb-7 w-[70vw] lg:w-[75vw] flex flex-row bg-transparent mr-6 lg:mr-0">
         <input
           type="text"
           className="w-full h-10 border-2 border-gray-100 rounded-l-lg p-4 text-lg placeholder:text-lg"
